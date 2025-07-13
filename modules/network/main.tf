d4747eb554c4942db492641f8b24bd8a7f5cece3
pinksour <man722156@gmail.com>
2025-07-14 03:00:27 +0900

    Network module: rename public_subnet_cidrs to public_subnet_ids and use input IDs

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -3,15 +3,6 @@ resource "aws_vpc" "this" {
   tags = { Name = "${var.name}-vpc" }
 }
 
-#resource "aws_subnet" "public" {
-#  for_each = zipmap(var.azs, var.public_subnet_cidrs)
-#  vpc_id                  = aws_vpc.this.id
-#  availability_zone       = each.key
-#  cidr_block              = each.value
-#  map_public_ip_on_launch = true
-#  tags = { Name = "${var.name}-public-${each.key}" }
-#}
-
 output "vpc_id" {
   value = aws_vpc.this.id
 }
