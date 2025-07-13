0090b922e0f77ba28bd8d19854ad4b0831397193
pinksour <man722156@gmail.com>
2025-07-14 05:12:46 +0900

    Fix NLB health_check:

--- modules/nlb/main.tf
+++ modules/nlb/main.tf
@@ -1,16 +1,3 @@
-variable "name"       { type = string }
-variable "subnet_ids" { type = list(string) }
-variable "vpc_id"     { type = string }
-variable "sg_nlb_id"  { type = string }
-variable "targets" {
-  type = map(object({
-    port              = number
-    protocol          = string
-    instance_ids      = list(string)
-    health_check_path = string
-  }))
-}
-
 locals {
   # var.targets 를 평탄화(flatten)해서 attachments 리스트 생성
   attachments = flatten([
